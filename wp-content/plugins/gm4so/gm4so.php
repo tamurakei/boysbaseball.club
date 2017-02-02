970863831063a65ee28daef988ae220287097552
田村啓 <tamura@shikumilab.jp>
2017-02-02 20:21:56 +0900

    DOM のロードを待って実行

--- wp-content/plugins/gm4so/gm4so.php
+++ wp-content/plugins/gm4so/gm4so.php
@@ -503,36 +503,37 @@ function gm4so_print_scripts_admin(){
 <script type="text/javascript">
 document.addEventListener( 'DOMContentLoaded', function() {
 // ページ読み込み完了時に実行する関数
-function init() {
+	function init() {
 
-	// 初期位置
-	var okayamaTheLegend = new google.maps.LatLng(34.666358, 133.918576);
+		// 初期位置
+		var okayamaTheLegend = new google.maps.LatLng(34.666358, 133.918576);
 
-	// マップ表示
-	var okayamap = new google.maps.Map(document.getElementById("map"), {
-		center: okayamaTheLegend,
-		zoom:13,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	});
+		// マップ表示
+		var okayamap = new google.maps.Map(document.getElementById("map"), {
+			center: okayamaTheLegend,
+			zoom:13,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		});
 
-	// ドラッグできるマーカーを表示
-	var marker = new google.maps.Marker({
-		position: okayamaTheLegend,
-		title: "Okayama the Legend!",
-		draggable: true	// ドラッグ可能にする
-	});
-	marker.setMap(okayamap)	;
+		// ドラッグできるマーカーを表示
+		var marker = new google.maps.Marker({
+			position: okayamaTheLegend,
+			title: "Okayama the Legend!",
+			draggable: true	// ドラッグ可能にする
+		});
+		marker.setMap(okayamap)	;
 
-	// マーカーのドロップ（ドラッグ終了）時のイベント
-	google.maps.event.addListener( marker, 'dragend', function(ev){
-		// イベントの引数evの、プロパティ.latLngが緯度経度。
-		document.getElementById('latitude').value = ev.latLng.lat();
-		document.getElementById('longitude').value = ev.latLng.lng();
-	});
-}
-}
-// ONLOADイベントにセット
-window.onload = init();
+		// マーカーのドロップ（ドラッグ終了）時のイベント
+		google.maps.event.addListener( marker, 'dragend', function(ev){
+			// イベントの引数evの、プロパティ.latLngが緯度経度。
+			document.getElementById('latitude').value = ev.latLng.lat();
+			document.getElementById('longitude').value = ev.latLng.lng();
+		});
+	}
+	// ONLOADイベントにセット
+	window.onload = init();
+});
+
 </script>
 
 EOL;
