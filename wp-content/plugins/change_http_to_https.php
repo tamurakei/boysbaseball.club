7dba96f45af65ceb299a9a9ebeed7f026067551b
田村啓 <tamura@shikumilab.jp>
2018-06-27 20:13:43 +0900

    Add Plugin http -> https

---
+++ wp-content/plugins/change_http_to_https.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Change http:// -> https://
+Plugin URI: http://www.shikumilab.jp
+Version: 1.0
+Description: replace http:// -> https://
+Author: Shikumilab.jp
+Author URI: http://www.shikumilab.jp
+*/
+
+function change_http_to_https ( $content ) {
+	str_replace('http://','https://',$content);
+	return $return ;
+}
+apply_filters( 'the_content', 'change_http_to_https' );
